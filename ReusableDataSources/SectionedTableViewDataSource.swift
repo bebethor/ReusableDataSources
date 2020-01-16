3278cb36d9b69e093197b76a3928e41fb5071d46
Jose Alberto Ruiz-Carrillo González <jose.alberto.rcg@gmail.com>
2020-01-16 16:04:21 +0100

    ADDING: Mocked sections

--- ReusableDataSources/SectionedTableViewDataSource.swift
+++ ReusableDataSources/SectionedTableViewDataSource.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
+struct Sections {
+  var title: String?
+ 
+  init(title: String?) {
+    self.title = title
+  }
+}
+
 class SectionedTableViewDataSource: NSObject {
     private let dataSources: [UITableViewDataSource]
+    var section1 = Sections(title: "Contacts")
+    var section2 = Sections(title: "Top Messages")
+    private var sections: [Sections] = []
     
     init(dataSources: [UITableViewDataSource]) {
         self.dataSources = dataSources
+        self.sections.append(contentsOf: [section1, section2])
     }
 }
 
 extension SectionedTableViewDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return dataSources.count
     }
